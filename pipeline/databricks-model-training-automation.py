ceb7e0e1622db4bbaba7ac744b4d11504d5fd5e7
Hossein Sarshar <hossein.sarshar@gmail.com>
2023-02-28 00:44:56 -0500

    adb fix

--- pipeline/databricks-model-training-automation.py
+++ pipeline/databricks-model-training-automation.py
@@ -11,14 +11,14 @@ db = DatabricksAPI(
 )
 
 db.repos.update_repo(
-    id = 3491573765963687,
+    id = 1518598551337928,
     branch='main'
 )
 
 job_id = db.jobs.create_job(
     name="ml-ops-model-training-{}".format(build_id),
     existing_cluster_id="0227-163903-wnphf6fm",
-    notebook_task={"notebook_path":"/Repos/hossein.sarshar@microsoft.com/databricks-aml-mlops-workshop/model-training"}
+    notebook_task={"notebook_path":"/Repos/hossein.sarshar@microsoft.com/databricks-aml-mlops-workshop"}
 )['job_id']
 
 run_id = db.jobs.run_now(
