89a576be7b1552b641bb3bed35c02d445dcad359
Hossein Sarshar <hossein.sarshar@gmail.com>
2023-02-28 00:52:05 -0500

    fixed

--- pipeline/databricks-model-training-automation.py
+++ pipeline/databricks-model-training-automation.py
@@ -18,7 +18,7 @@ db.repos.update_repo(
 job_id = db.jobs.create_job(
     name="ml-ops-model-training-{}".format(build_id),
     existing_cluster_id="0227-163903-wnphf6fm",
-    notebook_task={"notebook_path":"/Repos/hossein.sarshar@microsoft.com/databricks-aml-mlops-workshop"}
+    notebook_task={"notebook_path":"/Repos/hossein.sarshar@microsoft.com/databricks-aml-mlops-workshop/model_training"}
 )['job_id']
 
 run_id = db.jobs.run_now(
