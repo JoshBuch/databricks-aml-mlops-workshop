c80d4264d8636ea234eab4aab2079faa1789c0d5
Hossein Sarshar <hossein.sarshar@gmail.com>
2023-03-08 11:43:23 -0500

    parameters

--- aml/training/pipeline/main.py
+++ aml/training/pipeline/main.py
@@ -4,37 +4,47 @@ from azureml.pipeline.steps import PythonScriptStep
 from azureml.core.runconfig import RunConfiguration
 from azureml.pipeline.steps import PythonScriptStep
 from azureml.pipeline.core import PipelineParameter, StepSequence, Pipeline
-
+import argparse
 import os
 
-subscription_id = 'dac8073e-1c2d-4a7d-a53b-c3655e291d58'
-resource_group = 'Learning'
-workspace_name = 'learningmain'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--build-id', type=str, dest='build_id')
+    parser.add_argument('--workspace-name', type=str, dest="workspace_name")
+    parser.add_argument('--subscription-id', type=str, dest="subscription_id")
+    parser.add_argument('--resource-group', type=str, dest="resource_group")
+    
+    (args, extra_args) = parser.parse_known_args()
+    print('args', args)
 
-ws = Workspace(subscription_id, resource_group, workspace_name)
+    workspace_name = args.workspace_name
+    subscription_id = args.subscription_id
+    resource_group = args.resource_group
 
-aml_compute = AmlCompute(ws, "cpu-cluster")
+    ws = Workspace(subscription_id, resource_group, workspace_name)
 
-env = Environment('workshop-env')
+    aml_compute = AmlCompute(ws, "cpu-cluster")
 
-env.docker.base_image = 'mcr.microsoft.com/azureml/curated/sklearn-0.24-ubuntu18.04-py37-cpu:latest'
-env.python.user_managed_dependencies = True
+    env = Environment('workshop-env')
 
-# create a new runconfig object
-run_config = RunConfiguration()
-run_config.environment = env
+    env.docker.base_image = 'mcr.microsoft.com/azureml/curated/sklearn-0.24-ubuntu18.04-py37-cpu:latest'
+    env.python.user_managed_dependencies = True
 
-print(os.system('pwd'))
-print(os.system('ls -lah'))
+    # create a new runconfig object
+    run_config = RunConfiguration()
+    run_config.environment = env
 
-run_step = PythonScriptStep(script_name="main_script.py",
-                            compute_target=aml_compute,
-                            source_directory='aml/training/pipeline/program',
-                            runconfig = run_config,
-                            allow_reuse = False)
+    print(os.system('pwd'))
+    print(os.system('ls -lah'))
 
-steps = StepSequence(steps = [run_step])
-pipeline = Pipeline(workspace=ws, steps=steps)
-pipeline.validate()
+    run_step = PythonScriptStep(script_name="main_script.py",
+                                compute_target=aml_compute,
+                                source_directory='aml/training/pipeline/program',
+                                runconfig = run_config,
+                                allow_reuse = False)
 
-Experiment(ws, "PIPELINE_MAIN_Run").submit(pipeline)
+    steps = StepSequence(steps = [run_step])
+    pipeline = Pipeline(workspace=ws, steps=steps)
+    pipeline.validate()
+
+    Experiment(ws, "PIPELINE_MAIN_Run").submit(pipeline)
